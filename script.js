904183aff7995050ac51493ebe66893ce793c758
alexchavez97 <45809205+alexchavez97@users.noreply.github.com>
2019-04-03 11:54:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 "use strict";
 
-var words = ['purple', 'orange', 'family', 'twelve', 'silver', 'thirty', 'donate', 'people', 'future', 'heaven', 'banana', 'africa', 'monday', 'office', 'nature', 'eleven', 'animal', 'twenty', 'snitch', 'rachel', 'friday', 'father', 'yellow', 'poetry', 'august', 'broken', 'potato', 'sunday', 'circle', 'school', 'breath', 'moment', 'circus', 'person', 'scarce', 'energy', 'sister', 'spring', 'change', 'monkey', 'system', 'secret', 'pirate', 'turtle', 'ninety', 'mother'];
-
-var word1 = words[Math.floor(Math.random() * words.length)];
-var word2 = words[Math.floor(Math.random() * words.length)];
-var word3 = words[Math.floor(Math.random() * words.length)];
-var word4 = words[Math.floor(Math.random() * words.length)];
-var word5 = words[Math.floor(Math.random() * words.length)];
-var word6 = words[Math.floor(Math.random() * words.length)];
-var word7 = words[Math.floor(Math.random() * words.length)];
-var word8 = words[Math.floor(Math.random() * words.length)];
-var word9 = words[Math.floor(Math.random() * words.length)];
-var word10 = words[Math.floor(Math.random() * words.length)];
+var words = ['purple', 'orange', 'family', 'twelve',
+             'silver', 'thirty', 'donate', 'people',
+             'future', 'heaven', 'banana', 'africa',
+             'monday', 'office', 'nature', 'eleven',
+             'animal', 'twenty', 'snitch', 'rachel',
+             'friday', 'father', 'yellow', 'poetry',
+             'august', 'broken', 'potato', 'sunday',
+             'circle', 'school', 'breath', 'moment',
+             'circus', 'person', 'scarce', 'energy',
+             'sister', 'spring', 'change', 'monkey',
+             'system', 'secret', 'pirate', 'turtle',
+             'ninety', 'mother', 'winter', 'bucket',
+             'doctor', 'strong', 'forest', 'joyful',
+             'memory', 'season', 'friend', 'ending',
+             'loving', 'cookie', 'silent', 'talent',
+             'turkey', 'better', 'bottle', 'happen',
+             'wonder', 'number', 'little', 'spirit',
+             'melody', 'lovely', 'ginger', 'cousin',
+             'couple', 'bridge', 'tomato', 'enough',
+             'dragon', 'lonely', 'beaver', 'market',
+             'health', 'beauty', 'simple'];
 
 var scrambler = function(x){
   for (var i = 0; i < x.length; i++) {
@@ -21,6 +30,17 @@ var scrambler = function(x){
   }
   return x;
 };
+
+var word1 = (words[Math.floor(Math.random() * words.length)]);
+var word2 = (words[Math.floor(Math.random() * words.length)]);
+var word3 = (words[Math.floor(Math.random() * words.length)]);
+var word4 = (words[Math.floor(Math.random() * words.length)]);
+var word5 = (words[Math.floor(Math.random() * words.length)]);
+var word6 = (words[Math.floor(Math.random() * words.length)]);
+var word7 = (words[Math.floor(Math.random() * words.length)]);
+var word8 = (words[Math.floor(Math.random() * words.length)]);
+var word9 = (words[Math.floor(Math.random() * words.length)]);
+var word10 = (words[Math.floor(Math.random() * words.length)]);
 
 var adArray = ["ads/ad1.jpg",
                "ads/ad2.jpg",
@@ -162,14 +182,11 @@ $(document).ready(function(){
     }
   });
 
-  $(".stopbutton").click(function(){
-      $(".anagram").hide();
-      $("#endscreen").show();
-  });
 
   $(".button2").click(function(){
     $(".content").hide();
     $("#description").show();
+    $(".id").val($(".number").val())
   });
 
   $(".button3").click(function(){
@@ -185,7 +202,7 @@ $(document).ready(function(){
     $(".content").hide();
     $("#scramble2").show();
     $(".anagram2").html(scrambler(word2));
-    console.log(milToSec(timer.ticks()) + " seconds");
+    $(".a1").val(word1 + ", " + $(".answer1").val() + ", " + milToSec(timer.ticks()) + " seconds");
     realReset();
   });
 
@@ -193,7 +210,7 @@ $(document).ready(function(){
     $(".content").hide();
     $("#scramble3").show();
     $(".anagram3").html(scrambler(word3));
-    console.log(milToSec(timer.ticks()) + " seconds");
+    $(".a2").val(word2 + ", " + $(".answer2").val() + ", " + milToSec(timer.ticks()) + " seconds");
     realReset();
   });
 
@@ -201,74 +218,87 @@ $(document).ready(function(){
     $(".content").hide();
     $("#scramble4").show();
     $(".anagram4").html(scrambler(word4));
-    console.log(milToSec(timer.ticks()) + " seconds");
+    $(".a3").val(word3 + ", " + $(".answer3").val() + ", " + milToSec(timer.ticks()) + " seconds");
     realReset();
   });
 
   $(".button7").click(function(){
     $(".content").hide();
     $("#scramble5").show();
-    $(".anagram5").html(scrambler(word4));
-    console.log(milToSec(timer.ticks()) + " seconds");
+    $(".anagram5").html(scrambler(word5));
+    $(".a4").val(word4 + ", " + $(".answer4").val() + ", " + milToSec(timer.ticks()) + " seconds");
     realReset();
   });
 
   $(".button8").click(function(){
     $(".content").hide();
     $("#scramble6").show();
-    $(".anagram6").html(scrambler(word4));
-    console.log(milToSec(timer.ticks()) + " seconds");
+    $(".anagram6").html(scrambler(word6));
+    $(".a5").val(word5 + ", " + $(".answer5").val() + ", " + milToSec(timer.ticks()) + " seconds");
     realReset();
   });
 
   $(".button9").click(function(){
     $(".content").hide();
     $("#scramble7").show();
-    $(".anagram7").html(scrambler(word4));
-    console.log(milToSec(timer.ticks()) + " seconds");
+    $(".anagram7").html(scrambler(word7));
+    $(".a6").val(word6 + ", " + $(".answer6").val() + ", " + milToSec(timer.ticks()) + " seconds");
     realReset();
   });
 
   $(".button10").click(function(){
     $(".content").hide();
     $("#scramble8").show();
-    $(".anagram8").html(scrambler(word4));
-    console.log(milToSec(timer.ticks()) + " seconds");
+    $(".anagram8").html(scrambler(word8));
+    $(".a7").val(word7 + ", " + $(".answer7").val() + ", " + milToSec(timer.ticks()) + " seconds");
     realReset();
   });
 
   $(".button11").click(function(){
     $(".content").hide();
     $("#scramble9").show();
-    $(".anagram9").html(scrambler(word4));
-    console.log(milToSec(timer.ticks()) + " seconds");
+    $(".anagram9").html(scrambler(word9));
+    $(".a8").val(word8  + ", " + $(".answer8").val() + ", " + milToSec(timer.ticks()) + " seconds");
     realReset();
   });
 
   $(".button12").click(function(){
     $(".content").hide();
     $("#scramble10").show();
-    $(".anagram10").html(scrambler(word4));
-    console.log(milToSec(timer.ticks()) + " seconds");
+    $(".anagram10").html(scrambler(word10));
+    $(".a9").val(word9 + ", " + $(".answer9").val() + ", " + milToSec(timer.ticks()) + " seconds");
     realReset();
   });
 
   $(".button13").click(function(){
     $(".content").hide();
     $("#endscreen").show();
-    console.log(milToSec(timer.ticks()) + " seconds");
+    $(".a10").val(word10 + ", " + $(".answer10").val() + ", " + milToSec(timer.ticks()) + " seconds");
     timer.stop();
+    adTimer1.stop();
+    adTimer2.stop();
+    adTimer3.stop();
+    adTimer4.stop();
+    adTimer5.stop();
+    adTimer6.stop();
+    adTimer7.stop();
+    adTimer8.stop();
+    adTimer9.stop();
   });
 
+  $(".endbutton").click(function(){
+    $(".content").hide();
+    $("#formscreen").show();
+});
 
-    adTimer1.every(3000/8,function(){
+    adTimer1.every(3000,function(){
       $(".content").addClass("hidden");
       $(".ad1").attr("src",ad1);
       $("#advertisement1").show();
       timer.stop();
     });
 
-    adTimer1.every(6000/8,function(){
+    adTimer1.every(6000,function(){
       $("#advertisement1").hide();
       $(".ad1").removeAttr("src");
       adTimer1.stop();
@@ -276,14 +306,14 @@ $(document).ready(function(){
       adTimer2.start();
     });
 
-    adTimer2.every(3000/8,function(){
+    adTimer2.every(3000,function(){
       $(".content").addClass("hidden");
       $(".ad2").attr("src",ad2)
       $("#advertisement2").show();
       timer.stop();
     });
 
-    adTimer2.every(6000/8,function(){
+    adTimer2.every(6000,function(){
       $("#advertisement2").hide();
       $(".ad2").removeAttr("src");
       adTimer2.stop();
@@ -395,13 +425,160 @@ $(document).ready(function(){
       timer.start();
     });
 
+    $(".stopbutton").click(function(){
+        $(".anagram").hide();
+        $("#endscreen").show();
+        adTimer1.stop();
+        adTimer2.stop();
+        adTimer3.stop();
+        adTimer4.stop();
+        adTimer5.stop();
+        adTimer6.stop();
+        adTimer7.stop();
+        adTimer8.stop();
+        adTimer9.stop();
+    });
 
-
-    $(".closebutton").click(function(){
+    $(".closebutton1").click(function(){
       $("#advertisement1").hide();
       $("section").removeClass("hidden");
+      $(".adv1").val("closed, " + milToSec(adTimer1.ticks()) + " seconds")
       adTimer1.stop();
-
+      adTimer2.start();
   });
+
+    $(".interested1").click(function(){
+      $("#advertisement1").hide();
+      $("section").removeClass("hidden");
+      $(".adv1").val("interested, " + milToSec(adTimer1.ticks()) + " seconds")
+      adTimer1.stop();
+      adTime2.start();
+    });
+
+    $(".closebutton2").click(function(){
+      $("#advertisement2").hide();
+      $("section").removeClass("hidden");
+      $(".adv2").val("closed, " + milToSec(adTimer2.ticks()) + " seconds")
+      adTimer2.stop();
+      adTimer3.start();
+  });
+
+    $(".interested2").click(function(){
+      $("#advertisement2").hide();
+      $("section").removeClass("hidden");
+      $(".adv2").val("interested, " + milToSec(adTimer2.ticks()) + " seconds")
+      adTimer2.stop();
+      adTime3.start();
+    });
+
+    $(".closebutton3").click(function(){
+      $("#advertisement3").hide();
+      $("section").removeClass("hidden");
+      $(".adv3").val("closed, " + milToSec(adTimer3.ticks()) + " seconds")
+      adTimer3.stop();
+      adTimer4.start();
+  });
+
+    $(".interested3").click(function(){
+      $("#advertisement3").hide();
+      $("section").removeClass("hidden");
+      $(".adv3").val("interested, " + milToSec(adTimer3.ticks()) + " seconds")
+      adTimer3.stop();
+      adTime4.start();
+    });
+
+    $(".closebutton4").click(function(){
+      $("#advertisement4").hide();
+      $("section").removeClass("hidden");
+      $(".adv4").val("closed, " + milToSec(adTimer4.ticks()) + " seconds")
+      adTimer4.stop();
+      adTimer5.start();
+  });
+
+    $(".interested4").click(function(){
+      $("#advertisement4").hide();
+      $("section").removeClass("hidden");
+      $(".adv4").val("interested, " + milToSec(adTimer4.ticks()) + " seconds")
+      adTimer4.stop();
+      adTime5.start();
+    });
+
+    $(".closebutton5").click(function(){
+      $("#advertisement5").hide();
+      $("section").removeClass("hidden");
+      $(".adv5").val("closed, " + milToSec(adTimer5.ticks()) + " seconds")
+      adTimer5.stop();
+      adTimer6.start();
+  });
+
+    $(".interested5").click(function(){
+      $("#advertisement5").hide();
+      $("section").removeClass("hidden");
+      $(".adv5").val("interested, " + milToSec(adTimer5.ticks()) + " seconds")
+      adTimer5.stop();
+      adTime6.start();
+    });
+
+    $(".closebutton6").click(function(){
+      $("#advertisement6").hide();
+      $("section").removeClass("hidden");
+      $(".adv6").val("closed, " + milToSec(adTimer6.ticks()) + " seconds")
+      adTimer6.stop();
+      adTimer7.start();
+  });
+
+    $(".interested6").click(function(){
+      $("#advertisement6").hide();
+      $("section").removeClass("hidden");
+      $(".adv6").val("interested, " + milToSec(adTimer6.ticks()) + " seconds")
+      adTimer6.stop();
+      adTime7.start();
+    });
+
+    $(".closebutton7").click(function(){
+      $("#advertisement7").hide();
+      $("section").removeClass("hidden");
+      $(".adv7").val("closed, " + milToSec(adTimer7.ticks()) + " seconds")
+      adTimer7.stop();
+      adTimer8.start();
+  });
+
+    $(".interested7").click(function(){
+      $("#advertisement7").hide();
+      $("section").removeClass("hidden");
+      $(".adv7").val("interested, " + milToSec(adTimer7.ticks()) + " seconds")
+      adTimer7.stop();
+      adTime8.start();
+    });
+
+    $(".closebutton8").click(function(){
+      $("#advertisement8").hide();
+      $("section").removeClass("hidden");
+      $(".adv8").val("closed, " + milToSec(adTimer8.ticks()) + " seconds")
+      adTimer8.stop();
+      adTimer9.start();
+  });
+
+    $(".interested8").click(function(){
+      $("#advertisement8").hide();
+      $("section").removeClass("hidden");
+      $(".adv8").val("interested, " + milToSec(adTimer8.ticks()) + " seconds")
+      adTimer8.stop();
+      adTime9.start();
+    });
+
+    $(".closebutton9").click(function(){
+      $("#advertisement9").hide();
+      $("section").removeClass("hidden");
+      $(".adv9").val("closed, " + milToSec(adTimer9.ticks()) + " seconds")
+      adTimer9.stop();
+  });
+
+    $(".interested9").click(function(){
+      $("#advertisement9").hide();
+      $("section").removeClass("hidden");
+      $(".adv9").val("interested, " + milToSec(adTimer9.ticks()) + " seconds")
+      adTimer9.stop();
+    });
 
 });
